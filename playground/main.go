4718c471131aa21a881baa39de9385b7600cb78e
omoterikuto <omoterikuto>
2022-06-05 21:25:31 +0900

    hoge

--- playground/main.go
+++ playground/main.go
@@ -1,22 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func ping(pings chan<- string, msg string) {
-	pings <- msg
-}
-
-// この `pong` 関数は、1 つ目のチャネルを受信専用で (`pings`)、
-// 2 つ目のチャネルを送信専用で (`pongs`) 受け取ります。
-func pong(pings <-chan string, pongs chan<- string) {
-	msg := <-pings
-	pongs <- msg
-}
+var N int
+var A []int
 
 func main() {
-	pings := make(chan string, 1)
-	pongs := make(chan string, 1)
-	ping(pings, "Hello")
-	pong(pings, pongs)
-	fmt.Println(<-pongs)
+	file := "data.txt"
+	fp, err := os.Open(file)
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+
+	scanner := bufio.NewScanner(fp)
+
+	scanner.Scan()
+
+	nn, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	N = int(nn)
+
+	scanner.Scan()
+	line2 := strings.Split(scanner.Text(), " ")
+	for _, v := range line2 {
+		a, _ := strconv.ParseInt(v, 10, 64)
+		A = append(A, int(a))
+	}
+
+	var count int
+	for i := 0; i < N; i++ {
+		v := make([]int, 3)
+		v[0] = A[i]
+		for j := i + 1; j < N; j++ {
+			if v[0] == A[j] {
+				continue
+			}
+			v[1] = A[j]
+			for k := j + 1; k < N; k++ {
+				if v[0] == A[k] || v[1] == A[k] {
+					continue
+				}
+				count++
+			}
+		}
+	}
+	fmt.Println(count)
 }
