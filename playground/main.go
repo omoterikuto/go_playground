5849bcfb4fa52290c2cc4b88220eadcc4ebe23a7
omoterikuto <omoterikuto@gmail.com>
2022-12-22 00:06:35 +0900

    var3

--- playground/main.go
+++ playground/main.go
@@ -19,16 +19,13 @@ func main() {
 	b4 := makeMatrix()
 
 	var wg sync.WaitGroup
-	wg.Add(4)
-	b1t := transpose(b1)
-	b2t := transpose(b2)
-	b3t := transpose(b3)
-	b4t := transpose(b4)
+	wg.Add(3)
+
 	log.Println("start")
-	go calc(a1, b1t, &wg)
-	go calc(a2, b2t, &wg)
-	go calc(a3, b3t, &wg)
-	go calc(a4, b4t, &wg)
+	go calc(a1, b1, &wg)
+	go calc(a2, b2, &wg)
+	go calc(a3, b3, &wg)
+	calc(a4, b4, &wg)
 	wg.Wait()
 	log.Println("done")
 }
@@ -38,8 +35,8 @@ const n = 2048
 func calc(a1, b1 [n][n]int, wg *sync.WaitGroup) {
 	var p1 [n][n]int
 	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			for k := 0; k < n; k++ {
+		for k := 0; k < n; k++ {
+			for j := 0; j < n; j++ {
 				p1[i][j] += a1[i][k] * b1[k][j]
 			}
 		}
@@ -54,16 +51,6 @@ func makeMatrix() [n][n]int {
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
 			r[i][j] = rand.Intn(100)
-		}
-	}
-	return r
-}
-
-func transpose(p [n][n]int) [n][n]int {
-	var r [n][n]int
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			r[i][j] = p[i][j]
 		}
 	}
 	return r
